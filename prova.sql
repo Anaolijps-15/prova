cf56e4a70849b80cf8a221a247fe7e16d6d5201d
Anaolijps-15 <134750972+Anaolijps-15@users.noreply.github.com>
2024-05-14 18:21:17 -0300

    Add files via upload

---
+++ prova.sql
@@ -0,0 +1,18 @@
+SELECT quantidade_paginas,
+FROM Livros
+WHERE <600;
+
+update Livros
+set editora= 'Plume book'
+Where titulo = '1984'
+
+delete from livros where idioma= "Francês";
+
+select quantidade_paginas
+from Livros
+where = >600;
+
+select categoria
+from Livros
+where categoria=categoria
+group by Livros
